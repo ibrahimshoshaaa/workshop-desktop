c0693486d86259d12e718b1f73cc71d83dd3ba2b
Ibrahim Mohamed Shosha <71827080+ibrahimshoshaa@users.noreply.github.com>
2026-07-15 02:03:21 +0300

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -361,7 +361,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('ورشة التنجيد والأثاث - نسخة سطح المكتب', style: TextStyle(fontWeight: FontWeight.w600)),
+                Text('طاحون رويال هوم - نسخة سطح المكتب', style: TextStyle(fontWeight: FontWeight.w600)),
                 SizedBox(height: 4),
                 Text('الإصدار 1.0.0', style: TextStyle(color: Colors.grey)),
               ],
